56a540b1fcb0126bf234e81a61bd7d7d2869bd9b
keoo73 <moraher@zoho.com>
2021-11-26 16:35:16 -0500

    CompletarEntidades

--- EMigrant.App.Dominio/Entidades/TipoSector.cs
+++ EMigrant.App.Dominio/Entidades/TipoSector.cs
@@ -1,14 +1,13 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-
-namespace EMigrant.App.Dominio.Entidades
+namespace EMigrant.App.Dominio
 {
-    public class Sector
-    {
-        [Required(ErrorMessage = "El Campo es Requerido"), StringLength(15)]
-        public int Id { get; set;}
 
-        [Required(ErrorMessage = "El Nombre del Sector es requerido"), StringLength(15)]
-        public string Servicio { get; set;}
+    public enum TipoSector
+    {
+       publico,
+       privado,
+       sinAnimoDeLucro,
+       noGubernamental,
+       otro,
+       
     }
 }
